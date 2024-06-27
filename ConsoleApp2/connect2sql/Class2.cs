cf891ab2f5c766a8e54d0e7d260d7a001eb0fdea
Eren Topaçoğlu <93638566+Reactive21@users.noreply.github.com>
2024-06-27 10:59:03 +0300

    sql part

---
+++ ConsoleApp2/connect2sql/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp2.connect2sql
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string connectionString = "Your_Connection_String_Here";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Your SQL Server code goes here (e.g., INSERT statements to insert data into the database)
+            }
+        }
+    }
+}
